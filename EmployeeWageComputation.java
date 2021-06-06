16faf13e7d87d9fafa47e851ef1f96ddaf95c501
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-06 21:08:16 +0530

    Added: while loop by replacing for loop to calculate employee wage till a condition is met

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -16,7 +16,9 @@ class EmployeeWageComputation {
 	private static final int IS_PART_TIME = 2;
 	private static final int WAGE_RATE_PER_HOUR = 20;
 	private static final int WORKING_DAY_PER_MONTH = 20;
+	private static final int MAX_HOUR_IN_MONTH = 100;
 	private static int employeeMonthlyWage = 0;
+	private static int workRatePerHour = 0;
 	
 /**
  *  uc2- calculating daily wage for full time and part time employee
@@ -26,6 +28,7 @@ class EmployeeWageComputation {
 	private void employeeDailyWage( int workRatePerHour ) {
 		 int employeeDailyWage = WAGE_RATE_PER_HOUR * workRatePerHour;
 		 employeeMonthlyWage += employeeDailyWage;
+		 EmployeeWageComputation.workRatePerHour=workRatePerHour;
 		 System.out.println("Employee Daily wage is : " +employeeDailyWage +"$");
 	}
 	
@@ -36,13 +39,19 @@ class EmployeeWageComputation {
  *  passing work rate per hour value to employee wage calculation by creating object.
  *  uc3- added part time employee for calculating daily wage
  *  UC4- calculating daily wage using switch case	
- *  uc5- used for loop to iterate working days. 
+ *  uc5- used for loop to iterate working days and calculate wages 
+ *  UC6- used while loop to calculate  daily and monthly untill the condition is met.
+ *
  */		
 	public static void main(String[] args) {
+			
+			int totalEmployeeHours = 0;
+		    int totalWorkingDays = 0;
 
             System.out.println("Welcome to Employee Wage Computation Program");
 			Employee employee = new Employee();
-			for (int day = 0; day<workingDayPerMonth; day++ ){
+			
+			while (totalEmployeeHours <= MAX_HOUR_IN_MONTH && totalWorkingDays < WORKING_DAY_PER_MONTH ) {
 		    int attendanceCheck = (int)(Math.random()*3);
 	        switch (attendanceCheck){
 			case IS_FULL_TIME:
@@ -56,7 +65,10 @@ class EmployeeWageComputation {
 			default:
 			employee.employeeDailyWage(0);
 		     }
+			totalWorkingDays++;
+			totalEmployeeHours += workRatePerHour;
 		}
-		 System.out.println("Employee Monthly wage is :" +employeeMonthlyWage  +"$");
+		  System.out.println("Employee Total Hours is :" +totalEmployeeHours  +"$");
+	      System.out.println("Employee Monthly wage is :" +employeeMonthlyWage  +"$");
     }
 }
