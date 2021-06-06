07d7402208ecc10089b00ff7f5b5251e97dfb3ed
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-06 20:56:30 +0530

    Added: used for loop to calculate monthly employee wagee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -15,33 +15,34 @@ class EmployeeWageComputation {
 	private static final int IS_FULL_TIME = 1;
 	private static final int IS_PART_TIME = 2;
 	private static final int WAGE_RATE_PER_HOUR = 20;
+	private static final int WORKING_DAY_PER_MONTH = 20;
+	private static int employeeMonthlyWage = 0;
 	
 /**
  *  uc2- calculating daily wage for full time and part time employee
+ *  uc5 - calculating employee monthly wage.
  */	
 
 	private void employeeDailyWage( int workRatePerHour ) {
-		 int employeeDailyWage = wageRatePerHour * workRatePerHour;
+		 int employeeDailyWage = WAGE_RATE_PER_HOUR * workRatePerHour;
+		 employeeMonthlyWage += employeeDailyWage;
 		 System.out.println("Employee Daily wage is : " +employeeDailyWage +"$");
 	}
-
-   
+	
  /**
  *  This is the main method
  *  Displaying Welcome to Employee Wage Computation Program
- */
- 
-	public static void main(String[] args) {
-
-       System.out.println("Welcome to Employee Wage Computation Program");
-	   
-/**
  *  uc1- checking  if full time employee is present or absent
  *  passing work rate per hour value to employee wage calculation by creating object.
  *  uc3- added part time employee for calculating daily wage
  *  UC4- calculating daily wage using switch case	
+ *  uc5- used for loop to iterate working days. 
  */		
+	public static void main(String[] args) {
+
+            System.out.println("Welcome to Employee Wage Computation Program");
 			Employee employee = new Employee();
+			for (int day = 0; day<workingDayPerMonth; day++ ){
 		    int attendanceCheck = (int)(Math.random()*3);
 	        switch (attendanceCheck){
 			case IS_FULL_TIME:
@@ -55,5 +56,7 @@ class EmployeeWageComputation {
 			default:
 			employee.employeeDailyWage(0);
 		     }
+		}
+		 System.out.println("Employee Monthly wage is :" +employeeMonthlyWage  +"$");
     }
 }
