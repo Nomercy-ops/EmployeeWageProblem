35de22d3a3861aee52c786a05f28b61bcca7f841
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-08 18:08:40 +0530

    Added: ability to save the total wage for each company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,16 +2,77 @@
  *
  * @author Rikesh Chhetri
  * @version 1.0
- * @Created_on: 06.06.21
+ * @Created_on: 08.06.21
  *
- * purpose: is to calculate employee daily and monthly wages.
+ * purpose: is to calculate employee daily and monthly wages for the Multiple companies.
  *
  */
+ 
+ 
 package com.bridgelabz.Employeewage;
 
-public class EmployeeWageComputation {
+public class EmployeeWageComputation
+{
+    // class constants
+    private static final int PART_TIME = 1;
+    private static final int FULL_TIME = 2;
+   
+  
+    private final String companyName;
+    private final int wageRatePerHour;
+    private  final int numberOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmployeeWage;
+	
+	/**
+     * 
+     *  EmployeeWageComputation is the constructor created with arguments  for initialization of values.
+     * @param companyName
+     * @param wageRatePerHour
+     * @param numberOfWorkingDays
+     * @param maxHoursPerMonth
+     *
+     */
 
-    /**
+    EmployeeWageComputation(String companyName, int wageRatePerHour, int numberOfWorkingDays, int maxHoursPerMonth)
+    {
+        this.companyName = companyName;
+        this.wageRatePerHour= wageRatePerHour;
+        this.numberOfWorkingDays = numberOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;    
+    }
+	
+/**
+  * Method  for generating random number either 0 or 1 or 2.
+  */
+
+   private int generateEmployeeType()
+    {
+        return (int) (Math.random() * 100) % 3;
+    }
+	
+	
+ /**
+   *  Method  for generating Employee type either full time employee or parttime.
+   *  @param employeeType 
+   *  @return either 8 or 4 or 0 .
+   */
+
+    private int getWorkingHours(int employeeType)
+    {
+        switch (employeeType)
+        {
+            case FULL_TIME:
+                return 8;
+            case PART_TIME:
+                return 4;
+            default:
+                return 0;
+        }
+    }
+
+
+ /**
      * calculateTotalWage method gets value from main method and calculate the
      * employee wage
      *
@@ -21,50 +82,58 @@ public class EmployeeWageComputation {
      * @param maxWorkingHours
      *
      */
-    private static void calculateTotalWage(String companyName, int wageRatePerHour, int maxWorkingDays, int maxWorkingHours) {
-        //constant variable
-        final int PART_TIME = 1;
-        final int FULL_TIME = 2;
 
-        // instance variable
-        int totalWage = 0;
-        int workingHours = 0;
-        int totalWorkingHours = 0;
 
-        // for loop checking 
-        for (int day = 1; day <= maxWorkingDays && totalWorkingHours <= maxWorkingHours; day++) {
-            totalWorkingHours += workingHours;
-            int employeeType = (int) (Math.random() * 100) % 3;
-            switch (employeeType) {
-                case FULL_TIME:
-                    workingHours = 8;
-                    break;
-                case PART_TIME:
-                    workingHours = 4;
-                    break;
-                default:
-                    workingHours = 0;
-            }
-            int Employeewage = workingHours * wageRatePerHour;
-            totalWage += Employeewage;
+    private void calculateTotalWage()
+    {
+		// computation done here
+		
+        System.out.println("Computation of total wage of " + companyName + " employee");
+        int workingHours;
+        for (int day = 1, totalWorkingHours = 0; day <= numberOfWorkingDays
+                && totalWorkingHours <= maxHoursPerMonth; day++, totalWorkingHours += workingHours)
+        {
+            int employeeType = generateEmployeeType();
+            workingHours = getWorkingHours(employeeType);
+            int wage = workingHours * wageRatePerHour;
+            totalEmployeeWage += wage;
+			System.out.println("Day#: " + day +  " Working Hours: " + workingHours);
+        }
 
-            System.out.println("Day#: " + day + " Working Hours: " + workingHours);
-        } //end of for loop
-
-        System.out.println("Total wage for a month of " + companyName + " employee is " + totalWage + "\n");
+    }
+	
+	
+	
+	/** 
+	 *     This method returns the string itself 
+	 */
+	
+    public String toString()
+    {
+        return "Total wage of a month for " + companyName + " Employee is : " +totalEmployeeWage;
     }
 
-    /**
+ 
+ /**
      *
      * This is the main method Displaying Welcome to Employee Wage Computation
      * passing company name wage rate per hour into calculate total wage method
      * for employee wage
+	 * google and microsoft are the object that is created for EmployeeWageComputation.
      *
-     * @param args
      */
-    public static void main(String args[]) {
-        calculateTotalWage("Amazon", 40, 15, 200);
-        calculateTotalWage("BigBazar", 20, 20, 100);
-        calculateTotalWage("Dmart", 10, 15, 200);
+
+
+    public static void main(String args[])
+    {
+		
+        EmployeeWageComputation google = new EmployeeWageComputation("Google", 16, 20, 100);
+        EmployeeWageComputation microsoft = new EmployeeWageComputation("Microsoft", 12, 20, 150);
+
+        google.calculateTotalWage();
+        System.out.println(google);
+
+        microsoft.calculateTotalWage();
+        System.out.println(microsoft);
     }
 }
